870d9e813ab1ffcd39fcdd0501a302ed2cc33f30
Beth Ludojoski <beth@arte7.com.ar>
2023-10-04 19:27:56 -0300

    Commit Inicial 03-10-23

---
+++ src/main/java/proyecto/socialfashion/SeguridadWeb/WebSecurity.java
@@ -0,0 +1,12 @@
+package proyecto.socialfashion.SeguridadWeb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurity {
+    
+}
